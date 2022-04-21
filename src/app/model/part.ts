ad41f374ea32031652c327f5ff3b6606ef9d1b6c
IMATIA\to <antoniovazquezaraujo@gmail.com>
2022-04-21 13:34:50 +0200

    Fix graphic layout and style Fixes #5

--- src/app/model/part.ts
+++ src/app/model/part.ts
@@ -1,13 +1,14 @@
 import { Block } from "./block";
 export class Part{
-
-    public block:Block= new Block({});
+    static _id:number = 0;
+    
+    id=Part._id++;
+    block:Block= new Block({});
 
     constructor(opts?: Partial<Part>) {
         if (opts?.block != null) {
             this.block = new Block(opts.block);
         }
-
     }
     removeBlock(block:Block){
         this.block.children = this.block.children.filter(t => t!= block);
