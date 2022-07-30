f04c33cd404bfcef5ae05d859faed308d2836c06
IMATIA\to <antoniovazquezaraujo@gmail.com>
2022-07-30 10:58:02 +0200

    Remove dead code

--- src/app/model/song.ts
+++ src/app/model/song.ts
@@ -5,25 +5,11 @@ import { Part } from './part';
 export class Song {
     public parts?:Part[];
 
-    // constructor(parts:Part[]){
-        // this.parts = parts;
-    // }
-
     addPart():void{
         if(this.parts == null ){
             this.parts = [];
         }
         this.parts!.push(new Part());
     }
-    addCommand():void{
-        // var lastPart = this.getLastPart();
-        // if(lastPart.blocks.length == 0){
-        //     lastPart.blocks.push(new Block({commands:[], blockContent:{notes:''}}));
-        // }
-        // let lastBlock = lastPart.block;
-        // lastBlock.commands.push(new Command({commandType:CommandType.PULSE,commandValue:'50'}));
-    }
-    addNotes():void{
-        // this.getLastPart().blocks.push(new Block({commands:[], blockContent:{notes:''}}));
-    }
+   
 }
