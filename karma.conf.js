5fda70ee043bfe42b78660426b596e4de84965f4
IMATIA\to <antoniovazquezaraujo@gmail.com>
2022-08-21 18:54:48 +0200

    Renamed to moderanger

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/song-player'),
+      dir: require('path').join(__dirname, './coverage/moderanger'),
       subdir: '.',
       reporters: [
         { type: 'html' },
