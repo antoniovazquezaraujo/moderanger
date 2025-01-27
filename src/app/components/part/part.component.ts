7f4c142ace6e09938ad533df2d0bc5c037a9c337
antoniovazquezaraujo <antoniovazquezaraujo@gmail.com>
2025-01-27 20:09:20 +0100

    recover play part button

--- src/app/components/part/part.component.ts
+++ src/app/components/part/part.component.ts
@@ -4,6 +4,10 @@ import { Block } from 'src/app/model/block';
 import { Command } from 'src/app/model/command';
 import { Part } from 'src/app/model/part';
 import { VariableContext } from 'src/app/model/variable.context';
+import { SongPlayer } from 'src/app/model/song.player';
+import { InstrumentType } from 'src/app/model/instruments';
+import { Song } from 'src/app/model/song';
+import { Player } from 'src/app/model/player';
 
 @Component({
     selector: 'app-part',
@@ -18,7 +22,7 @@ export class PartComponent implements OnInit {
     @Output() onDuplicatePart: EventEmitter<Part>;
     @Output() onRemovePart: EventEmitter<Part>;
 
-    constructor() {
+    constructor(private songPlayer: SongPlayer) {
         this.onDuplicatePart = new EventEmitter<Part>();
         this.onRemovePart = new EventEmitter<Part>();
     }
@@ -56,5 +60,18 @@ export class PartComponent implements OnInit {
 
     removePart() {
         this.onRemovePart.emit(this.part);
+    }
+
+    playPart() {
+        const player = new Player(0, this.part.instrumentType || InstrumentType.PIANO);
+        const dummySong = new Song();
+        if (this.variableContext) {
+            dummySong.variableContext = this.variableContext;
+        }
+        this.songPlayer.playPart(this.part, player, dummySong);
+    }
+
+    stopPart() {
+        this.songPlayer.stop();
     }
 }
