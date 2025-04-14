925b007fad6d2c9cedc8a6f8d04b253796f1be59
antoniovazquezaraujo <antoniovazquezaraujo@gmail.com>
2025-04-14 15:03:34 +0200

    remove memory lick

--- src/app/components/song-editor/song-editor.component.ts
+++ src/app/components/song-editor/song-editor.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, Input } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, Input, OnDestroy } from '@angular/core';
 import { Song } from 'src/app/model/song';
 import { Part } from 'src/app/model/part';
 import { SongPlayer } from 'src/app/model/song.player';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'app-song-editor',
@@ -10,7 +11,7 @@ import { Observable } from 'rxjs';
     styleUrls: ['./song-editor.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SongEditorComponent implements OnInit {
+export class SongEditorComponent implements OnInit, OnDestroy {
     @Input() song: Song = new Song();
     
     repetitions: number = 1;
@@ -18,22 +19,31 @@ export class SongEditorComponent implements OnInit {
     variablesSidebarVisible: boolean = false;
     isPlaying: boolean = false;
 
+    private destroy$ = new Subject<void>();
+
     constructor(
         private songPlayer: SongPlayer,
         private cdr: ChangeDetectorRef
     ) {
         this.metronome$ = this.songPlayer.metronome$;
-        this.metronome$.subscribe(() => {
-            const playing = this.songPlayer.isPlaying;
-            if (this.isPlaying !== playing) {
-                this.isPlaying = playing;
-                this.cdr.detectChanges();
-            }
-        });
+        this.metronome$
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(() => {
+                const playing = this.songPlayer.isPlaying;
+                if (this.isPlaying !== playing) {
+                    this.isPlaying = playing;
+                    this.cdr.detectChanges();
+                }
+            });
     }
 
     ngOnInit(): void {
         this.cdr.detectChanges();
+    }
+
+    ngOnDestroy(): void {
+        this.destroy$.next();
+        this.destroy$.complete();
     }
 
     toggleVariablesSidebar(): void {
